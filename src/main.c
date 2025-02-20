f78b05e384fa402b6343948f02624bfccc1ec6c0
Edwyn <edwynjesus30@example.com>
2025-02-20 01:33:13 -0400

    formato de ejecuciòn implementado

--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "utils.h"
 
-int main() {
-    printf("Hello, World!\n");
+// Variable global para almacenar la ruta del archivo
+char *filePath = NULL;
+
+
+// Obtiene los argumentos de la linea de comandos y los valida para que sean correctos
+// @param argc Cantidad de argumentos
+// @param argv Arreglo de argumentos
+void getArguments(int argc, char *argv[]) {
+    // Verifica que los argumentos sean correctos
+    if (argc == 4 && strcmp(argv[1], "-m") == 0 && strcmp(argv[2], "d") == 0) {
+        filePath = argv[3]; // Obtiene el nombre del archivo
+        printf("El nombre del archivo es: %s\n", filePath);
+    } else {
+        // Si los argumentos no son válidos, imprime el formato correcto y termina el programa
+        fprintf(stderr, "Formato correcto: ./deteccion -m d <nombre del archivo>\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    getArguments(argc, argv);
+    // Abre el archivo
     return 0;
 }
